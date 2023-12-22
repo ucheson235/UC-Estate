f1062eff667afb5cccbdc9c47a9206482a0e467b
ucheson235 <emmyboi2010@yahoo.com>
2023-12-22 18:03:58 +0100

    create login, sigin up, forgetpassword pages and oauth

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,7 +1,98 @@
 import React from 'react'
+import { useState } from 'react';
+import { IoMdEyeOff } from "react-icons/io";
+import { IoEye } from "react-icons/io5";
+import { Link } from 'react-router-dom';
+import OAuth from '../components/OAuth';
+
 
 export default function SignUp() {
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  const {name,email, password} = formData;
+  function onChange(e){
+    console.log(e.target.value);
+    setFormData((prevState)=>({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }))
+  }
+
   return (
-    <div>Sign Up</div>
+    <section>
+      <h1 className='text-3xl text-center mt-6 font-bold'>Sign Up</h1>
+      <div className='flex justify-center flex-wrap items-center px-6 py-12 max-w-6xl mx-auto'> 
+         <div className='md:w-[67%] lg:w-[50%] mb-12 md:mb-6'>
+             <img src='https://images.unsplash.com/flagged/photo-1564767609342-620cb19b2357?q=80&w=1973&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='Key' className='w-full rounded-2xl'/> 
+         </div>
+         <div className='w-full md:w-[67%] lg:w-[40%] lg:ml-20'>
+            <form >
+               <input 
+                   className='w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out mb-6' 
+                   type='text' 
+                   id='name' 
+                   value={name} 
+                   onChange={onChange}
+                   placeholder='Full Name' 
+                   
+                />
+               <input 
+                   className='w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out mb-6' 
+                   type='email' 
+                   id='email' 
+                   value={email} 
+                   onChange={onChange}
+                   placeholder='Email address'
+                   
+                />
+              <div className='relative mb-6'>
+                <input
+                  type={showPassword ? "text" : "password"}
+                  id='password'
+                  value={password} 
+                  onChange={onChange}
+                  placeholder='Password'
+                  className='w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out'  
+                  
+                    
+                   
+                />
+                {showPassword ? (
+                  <IoMdEyeOff className='absolute right-3 top-3 text-xl cursor-pointer' onClick={()=>setShowPassword((prevState) => ! prevState)} />
+                ):(
+                  <IoEye className='absolute right-3 top-3 text-xl cursor-pointer' onClick={()=>setShowPassword((prevState) => ! prevState)} />
+                  )}
+                </div>
+                <div className='flex justify-between whitespace-nowrap text-sm sm:text-lg'>
+                   <p className='mb-6'>
+                      Have an account ?
+                    <Link to="/Login" className='text-red-600 hover:text-red-700 transition duration-200 ease-in-out ml-1'> Login</Link>
+                   </p>
+
+                  <p>
+                   <Link to="/forgot-password" className='text-blue-600 hover:text-blue-800 transition duration-200 ease-in-out '>Forgot-password?</Link>
+          
+                  </p>
+                </div>
+                <button className='w-full bg-blue-600 text-white px-7 py-3 text-sm font-medium uppercase rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active-bg-blue-800' 
+                    ype='submit'>
+                    SignUp
+                </button>
+                <div className='flex items-center my-4 before:border-t  before:flex-1  before:border-gray-300 after:border-t  after:flex-1  after:border-gray-300'>
+                  <p className='text-center font-semibold mx-4'>OR</p>
+                </div>
+                <OAuth/>
+            </form>
+            
+         </div>
+
+      </div>
+    </section>
   )
 }
+
+
